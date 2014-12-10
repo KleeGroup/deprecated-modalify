c54e3ee10fa9a4d1408bbda00bd16472cbe199fb
Pierre Besson <pierre.besson7@gmail.com>
2014-12-10 22:05:13 +0100

    Fix url on backbone.

--- dist/modalify.js
+++ dist/modalify.js
@@ -31,39 +31,39 @@ var isInit = false;
  * @param  {object} options - Overriding options.
  * @return {Boolean} - true if the initialization occurs / false otherwise.
  */
-function init(options){
-  options = options || {};
-  if(isInit){
-    return false;
+function init(options) {
+    options = options || {};
+    if (isInit) {
+      return false;
+    }
+    var template = options.template || templateModal;
+    var selector = options.selector || conf.selector;
+    document.querySelector(selector).appendChild(domElement(template()));
+    var openElement = document.createElement('a');
+    openElement.href = "#modalify-container";
+    openElement.setAttribute('data-modalify-action', '');
+    document.body.insertBefore(openElement, null);
+    document.querySelector(selector).querySelector('[data-modalify-close]').addEventListener('click', function(event) {
+      console.log('close modal');
+      location.hash = url;
+    });
+    //events.hashUrl.cancelUrlChangeCssTarget();
+    isInit = true;
+    return true;
   }
-  var template = options.template || templateModal;
-  var selector = options.selector || conf.selector;
-  document.querySelector(selector).appendChild(domElement(template()));
-  var openElement = document.createElement('a');
-  openElement.href = "#modalify-container";
-  openElement.setAttribute('data-modalify-action', '');
-  document.body.insertBefore(openElement, null);
-  document.querySelector(selector).querySelector('[data-modalify-close]').addEventListener('click', function(event){
-    console.log('close modal');
-    location.hash = url;
-  });
-  //events.hashUrl.cancelUrlChangeCssTarget();
-  isInit = true;
-  return true;
-}
-/**
- * Add a DOM element in the modal.
- * @param {object} element - el:DOM element, container: .
- */
-function addElement(element){
-  if(!element.el){
+  /**
+   * Add a DOM element in the modal.
+   * @param {object} element - el:DOM element, container: .
+   */
+function addElement(element) {
+  if (!element.el) {
     throw new Error("There should be an el DOM element.");
   }
-  if(!element.closeSelector){
+  if (!element.closeSelector) {
     throw new Error("There should be a  close Selector");
   }
 
-  
+
 
   /*var closeElement = element.el.querySelector(element.closeSelector);
   if(closeElement === undefined ||  closeElement.tagName !== "A"){
@@ -75,35 +75,41 @@ function addElement(element){
   var closeElement = document.createElement('a');
   closeElement.href = "#modalify-close";
   closeElement.setAttribute('data-modalify-action', '');
-  if(!isInit){init();}
-  if(element.title){
+  if (!isInit) {
+    init();
+  }
+  if (element.title) {
     document.querySelector("[data-modalify-title]").innerHTML = element.title || '';
   }
   var modalContent = document.querySelector("[data-modalify-content]");
   modalContent.innerHTML = "";
   modalContent.insertBefore(element.el, null);
   openModal();
- // document.body.insertBefore(openElement, null);
+  // document.body.insertBefore(openElement, null);
   //Reregister dom events hash changing.
- // events.hashUrl.cancelUrlChangeCssTarget();
- // events.closeModal.closeModalAction(element.closeSelector);
+  // events.hashUrl.cancelUrlChangeCssTarget();
+  // events.closeModal.closeModalAction(element.closeSelector);
 
 }
 
-var openModal =  function(){
-    url = location.hash;
-    document.querySelector('a[href="#modalify-container"]').click();
-  };
+var openModal = function() {
+  url = location.hash;
+  document.querySelector('a[href="#modalify-container"]').click();
+};
 
 module.exports = {
   init: init,
   addElement: addElement,
-  close: function(){
-    
-    document.querySelector('a[href="#modalify-close"]').click();
-    location.hash = url;
+  close: function() {
+    document.querySelector('a[data-modalify-close]').click();
+    if (Backbone) {
+      Backbone.history.navigate(url);
+    } else {
+      location.hash = url;
+    }
+
   },
-  open:openModal
+  open: openModal
 };
 },{"./config.json":3,"./events":6,"./templates/modal":7,"./util/domElement":8}],3:[function(require,module,exports){
 module.exports={
